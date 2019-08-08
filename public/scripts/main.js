3b2cfca04a2a72c79b50cb3cc5e644242c1365c0
Jason Byer <jbyer3@gmail.com>
2019-08-08 18:48:01 -0400

    hotfix

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,23 +1,21 @@
-const frets = document.querySelectorAll('.fret')
-
-const fretArray = Array.from(frets)
+const frets = document.querySelectorAll('.fret');
+const fretArray = Array.from(frets);
 
 fretArray.forEach(fret => {
-  fret.addEventListener('click',
-    logger)
-})
+  fret.addEventListener('click', logger);
+});
 
 function logger() {
   
   fretArray.forEach(fret => {
     if(fret.dataset.notevalue === this.dataset.notevalue){
-      console.log(fret)
+      console.log(fret);
       if(!fret.classList.contains('selected')){
-        fret.classList.add('selected')
+        fret.classList.add('selected');
       } else {
-        fret.classList.remove('selected')
+        fret.classList.remove('selected');
       }
     }
-  })
+  });
 }
 
