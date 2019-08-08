141dab33a08c491bb3bdcd357ed5a03549f0f2c7
Jason Byer <jbyer3@gmail.com>
2019-08-08 15:26:41 -0400

    server and main page start

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => res.send('/public/index.html'));
+
+app.listen(port, () => console.log(`Express server listening on port ${port}`))
